d9a381944f920028a153ad628f0926444422f5a2
tensymb0ls <tensym8ols@gmail.com>
2023-08-27 20:09:03 +0300

    test

--- event_loop/script.js
+++ event_loop/script.js
@@ -9,15 +9,3 @@ setTimeout(() => {
     console.log('1.5 sec');
 }, 1500);
 console.log('end');
-
-function getInfo(member, year) {
-    member.name = "Lydia";
-    year = "1998";
-}
-
-const person = { name: "Sarah" };
-const birthYear = "1997";
-
-getInfo(person, birthYear);
-
-console.log(person, birthYear);
